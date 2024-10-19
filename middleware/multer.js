a2b73a0ebca7b32d95a3f601ad7234c5e12a8f31
Ketan Dudka <ketandudka566123@gmail.com>
2024-10-19 15:35:52 +0530

    push

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,13 +1,18 @@
 const multer = require ("multer")
 const path = require("path")
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require("../utils/cloudinary")
 
-const storage = multer.diskStorage({
-    destination: "./productImages/", // Directory where the file will be saved
+
+
+const storage = new CloudinaryStorage({
+    cloudinary: cloudinary,
+    folder: 'productImages', // Name of the folder in Cloudinary
+    allowedFormats: ['jpg', 'png', 'jpeg'], // Allowed file formats
     filename: (req, file, cb) => {
-      cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
     },
-  });
-  
+});
   const upload = multer({
     storage: storage,
   });
